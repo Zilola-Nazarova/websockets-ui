5b191d24482cf8db15434118875d262e758c4457
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-10-29 03:00:44 +0500

    feat: create websocket server

--- src/http_server/index.js
+++ src/http_server/index.js
@@ -1,17 +1,39 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
+import { WebSocketServer, WebSocket } from 'ws';
 
 export const httpServer = http.createServer(function (req, res) {
-    const __dirname = path.resolve(path.dirname(''));
-    const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-    fs.readFile(file_path, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
-        }
-        res.writeHead(200);
-        res.end(data);
+  const __dirname = path.resolve(path.dirname(''));
+  const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
+  fs.readFile(file_path, function (err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+});
+
+const wsServer = new WebSocketServer({
+  noServer: true
+});
+
+wsServer.on('connection', function(ws) {
+  console.log('WS Connected!');
+
+  ws.on('message', function(msg) {
+    console.log(`Received message: ${msg}`);
+    wsServer.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(`Server received your message: ${msg}`);
+      }
     });
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
 });
