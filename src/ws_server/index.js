0756b167d4c8926eb4096d2d52421e8bd21ed2a7
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-11-03 22:44:43 +0500

    feat: update websocket response messages and setup

--- src/ws_server/index.js
+++ src/ws_server/index.js
@@ -11,6 +11,7 @@ import updateRoom from '../controllers/updateRoom.js';
 import createRoom from '../controllers/createRoom.js';
 import addShips from '../controllers/addShips.js';
 import addUserToRoom from '../controllers/addUserToRoom.js';
+import { v4 as uuidv4 } from 'uuid';
 
 const WS_PORT = process.env.WS_PORT;
 
@@ -18,66 +19,132 @@ const wsServer = new WebSocketServer({
   port: WS_PORT
 });
 
+const lookup = {};
+
 wsServer.on('connection', function(ws) {
   console.log('WS Connected!');
+  ws.id = uuidv4();
+  lookup[ws.id] = ws;
 
   ws.on('message', function(msg) {
     console.log(`Received message: ${msg}`);
-    const msgJSON = JSON.parse(msg);
-    const dataJSON = JSON.parse(msgJSON.data);
-    let responseData;
-    switch (msgJSON.type) {
+    let { type, data, id } = JSON.parse(msg);
+    data = data ? JSON.parse(data) : "";
+    let regData, roomsData, winnersData, response;
+    switch (type) {
       case 'reg':
-        responseData = reg(msgJSON.type, dataJSON, msgJSON.id);
-        break;
-      case 'create_game':
-        responseData = createGame(msgJSON);
-        break;
-      case 'start_game':
-        responseData = startGame(msgJSON);
-        break;
-      case 'turn':
-        responseData = turn(msgJSON);
-        break;
-      case 'attack':
-        responseData = attack(msgJSON);
-        break;
-      case 'random_attack':
-        responseData = randomAttack(msgJSON);
-        break;
-      case'finish':
-        responseData = finish(msgJSON);
-        break;
-      case 'update_winners':
-        responseData = updateWinners(msgJSON);
+        regData = reg(id, data, ws.id);
+        roomsData = updateRoom(id);
+        winnersData = updateWinners(id);
+        lookup[ws.id].send(JSON.stringify(regData));
+        lookup[ws.id].send(JSON.stringify(roomsData));
+        lookup[ws.id].send(JSON.stringify(winnersData));
         break;
       case 'create_room':
-        responseData = createRoom(msgJSON);
-        break;
-      case 'update_room':
-        responseData = updateRoom(msgJSON);
-        break;
-      case 'add_ships':
-        responseData = addShips(msgJSON);
+        createRoom();
+        roomsData = updateRoom(id);
+        wsServer.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(roomsData));
+          }
+        });
         break;
       case 'add_user_to_room':
-        responseData = addUserToRoom(msgJSON);
+        const roomUsers = addUserToRoom(data.indexRoom, ws.id);
+        if (roomUsers.length === 2) {
+          const idGame = createGame(id, data.indexRoom);
+          wsServer.clients.forEach((client) => {
+            if (client.id === roomUsers[0].id || client.id === roomUsers[1].id) {
+              client.send(JSON.stringify({
+                type: "create_game",
+                data: JSON.stringify({ idGame, idPlayer: client.id}),
+                id
+              }));
+            }
+          });
+        }
+        roomsData = updateRoom(id);
+        wsServer.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(roomsData));
+          }
+        });
+        break;
+      case 'add_ships':
+        const readyState = addShips(data);
+        if (readyState) {
+          response = startGame(data);
+          response.players.forEach((player) => {
+            lookup[player.id].send(JSON.stringify({
+              type: "start_game",
+              data: JSON.stringify({ ships: player.ships, currentPlayerIndex: player.id }),
+              id
+            }));
+          });
+          response.players.forEach((player) => {
+            lookup[player.id].send(JSON.stringify({
+              type: "turn",
+              data: JSON.stringify({ currentPlayer: response.players[0].id }),
+              id
+            }));
+          });
+        }
+        break;
+      case 'attack':
+        response = attack(data);
+        response.players.forEach((player) => {
+          lookup[player.id].send(JSON.stringify({
+            type: "attack",
+            data: JSON.stringify({ ...response.feedback, currentPlayer: data.indexPlayer }),
+            id
+          }));
+        });
+        response.players.forEach((player) => {
+          lookup[player.id].send(JSON.stringify({
+            type: "turn",
+            data: JSON.stringify({
+              currentPlayer: response.players.find((player) => player.id !== data.indexPlayer).id
+            }),
+            id
+          }));
+        });
+        break;
+      case 'randomAttack':
+        response = randomAttack(data);
+        response.players.forEach((player) => {
+          lookup[player.id].send(JSON.stringify({
+            type: "attack",
+            data: JSON.stringify({ ...response.feedback, currentPlayer: data.indexPlayer }),
+            id
+          }));
+        });
+        if (!win) {
+          response.players.forEach((player) => {
+            lookup[player.id].send(JSON.stringify({
+              type: "turn",
+              data: JSON.stringify({
+                currentPlayer: response.players.find((player) => player.id !== data.indexPlayer).id
+              }),
+              id
+            }));
+          });
+        } else {
+          response.players.forEach((player) => {
+            lookup[player.id].send(JSON.stringify({
+              type: "finish",
+              data: finish(),
+              id
+            }));
+          });
+          winnersData = updateWinners(id);
+          response.players.forEach((player) => {
+            lookup[player.id].send(JSON.stringify(winnersData));
+          });
+        }
         break;
       default:
         console.log('Unknown message type');
     }
-
-    console.log(`Response message: ${JSON.stringify(responseData)}`);
-
-    wsServer.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(`Server received your message: ${msg}`);
-      }
-      if (client == ws) {
-        console.log('Sending response');
-        client.send(JSON.stringify(responseData));
-      }
-    });
   });
 
   ws.on('close', () => {
